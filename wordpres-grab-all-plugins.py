082efc4f874675b12d3404da8a83e10c7923aae8
livewireza <92034037+livewireza@users.noreply.github.com>
2025-08-12 16:29:19 +0200

    Create wordpres-grab-all-plugins.py
    wp plugins

---
+++ wordpres-grab-all-plugins.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib.request as hyperlink
+import os
+link =
+hyperlink.urlopen('http: /plugins.svn.wordpress.o
+rg/')
+soup = BeautifulSoup(link, 'lxml')
+Themes:
+(Same logic, different URL:
+http: /themes.svn.wordpress.org/ )
+Comprehensive testing of authentication mechanisms often
+reveals issues like open redirects, XSS, broken logic, or
+parameter manipulation vulnerabilities.
+file_path =
+os.path.dirname(os.path.realpath(__file__))
+output_file = os.path.join(file_path,
+'scrapedlist.txt')
+with open(output_file, 'wt', encoding='utf8') as
+file:
+for link in soup.find_all('a', href=True):
+slug = link.get('href').replace("/", "")
+file.write(slug + '\n')
+print(slug)
